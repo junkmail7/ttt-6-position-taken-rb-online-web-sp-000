14f3d553840572775bfecd29b80a1d0eb4cb5fa8
William Pannell <lpann5@gmail.com>
2020-10-30 00:43:41 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,4 +1,9 @@
 # code your #position_taken? method here!
-def position_taken?
+def position_taken?(board, index)
+  if board(index) == " "
+    return false
+  else
+    return true
+  end
   
 end
